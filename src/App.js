bc53fbf929de3e17c0187ab7441aa7015d6521e4
danielbelle <henrique.danielb@gmail.com>
2024-02-14 22:14:00 -0300

    v0.1 - adding Main, Navbar, Row, API requests and Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
